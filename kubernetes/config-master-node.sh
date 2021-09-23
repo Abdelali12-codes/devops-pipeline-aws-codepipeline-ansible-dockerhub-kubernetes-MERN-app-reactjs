806959291d744880388fc8ec0cc0234c15cf37bf
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-09-23 11:53:18 +0100

    new files

--- kubernetes/config-master-node.sh
+++ kubernetes/config-master-node.sh
@@ -94,16 +94,18 @@ kubeadm init --config /etc/kubernetes/aws.yaml
 
 
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# mkdir -p $HOME/.kube
+# sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
+# kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
+# kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+# kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
-sleep 60
+# sleep 60
 
-kubectl get nodes
+# kubectl get nodes
 
-echo " K8s Master Setup has completed"
+# echo " K8s Master Setup has completed"
 # Setup create Token
 # kubeadm token create --print-join-command
--- kubernetes/config-worker-node.sh
+++ kubernetes/config-worker-node.sh
@@ -74,22 +74,6 @@ sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 
-# Join with Master Node with AWS provider
-#cat << EOF > /etc/kubernetes/node.yaml
-#apiVersion: kubeadm.k8s.io/v1beta1
-#kind: JoinConfiguration
-#discovery:
-#  bootstrapToken:
-#    token: "3yeyro.3h7uq3v5d1i1l3ic"
-#    apiServerEndpoint: "172.31.87.129:6443"
-#    caCertHashes:
-#      - "sha256:b6988478aa2218c6ebe91bb68ba8365520bb6cba43adb797308340e51ed8bd9f"
-#nodeRegistration:
-#  name: ip-172-31-88-20.ec2.internal
-#  kubeletExtraArgs:
-#    cloud-provider: aws
-#EOF
 
-# kubeadm join --config /etc/kubernetes/node.yaml
 
 
