64b530493a4ee4dc15e537c08ca2cd6b779cc562
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-09-24 14:37:33 +0100

    new fiels

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -2,4 +2,4 @@ cd /home/ali/devops
 sudo cp -r build/* nginx
 
 sudo cp ansible/deployments-services.yml /home/ali/
-# sudo scp ansible/deployments-services.yml ubuntu@x.x.x.x:/home/ubuntu/cluster
+sudo scp ansible/deployments-services.yml ubuntu@10-10-1-69:/home/ubuntu/cluster
