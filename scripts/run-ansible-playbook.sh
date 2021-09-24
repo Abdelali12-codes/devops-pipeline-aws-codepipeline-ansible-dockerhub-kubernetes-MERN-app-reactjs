7e68743cb97c6936367f4b4a707f749eb19ef112
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-09-24 12:24:16 +0100

    new fiels

--- scripts/run-ansible-playbook.sh
+++ scripts/run-ansible-playbook.sh
@@ -1,4 +1,4 @@
 cd /home/abdelali/reactjs/ansible
 ansible-palybook ansible-build-nodejs-image.yml 
 ansible-palybook ansible-build-reactjs-image.yml 
-ansible-palybook ansible-run-cluster.yml
+# ansible-palybook ansible-run-cluster.yml
